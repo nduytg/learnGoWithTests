16ad09ae2adc130421800111505c1ac6f4a7f752
duy.nguyen <duy.nguyen@shopee.com>
2019-06-24 12:15:45 +0800

    Add maps tutorial

---
+++ maps/dictionary.go
@@ -0,0 +1,57 @@
+package maps
+
+import "errors"
+
+var (
+	ErrNotFound          = errors.New("could not find the word you were looking for")
+	ErrWordExists        = errors.New("cannot add word because it already exists")
+	ErrWordDoesNotExists = errors.New("cannot update word because it does not exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExists
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
